016a223f36a3330ddca487bdc1a9c2d3c7d9f240
notatent@gmail.com <notatent@gmail.com>
2020-06-07 23:21:04 +0800

    optimize

--- src/main/java/com/mean/world/a021_merge2SortedLists/Solution.java
+++ src/main/java/com/mean/world/a021_merge2SortedLists/Solution.java
@@ -1,7 +1,5 @@
 package com.mean.world.a021_merge2SortedLists;
 
-import java.util.ArrayList;
-
 class Solution {
 
     public static void main(String[] args) {
@@ -17,48 +15,54 @@ class Solution {
     }
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        return loop4Collection(l1, l2);
+        return loop(l1, l2);
     }
 
-    private ListNode loop4Collection(ListNode l1, ListNode l2) {
+    /**
+     * comment out code performance bad
+     */
+    private ListNode loop(ListNode l1, ListNode l2) {
         if (l1 == null) {
             return l2;
         }
         if (l2 == null) {
             return l1;
         }
-        ArrayList<ListNode> allNodes = new ArrayList<>();
-        ListNode smaller = null;
+        //ArrayList<ListNode> allNodes = new ArrayList<>();
+        //ListNode smaller = null;
         boolean has = true;
         ListNode current = new ListNode();
         ListNode sentinelNode = current;
         while (has) {
             if (l1.val <= l2.val) {
-                smaller = l1;
+                current.next = l1;
+                current = current.next;
                 l1 = l1.next;
                 if (l1 == null) {
-                    smaller.next = l2;
+                    current.next = l2;
                     l2 = null;
                 }
             } else {
-                smaller = l2;
+                current.next = l2;
+                current = current.next;
                 l2 = l2.next;
                 if (l2 == null) {
-                    smaller.next = l1;
+                    current.next = l1;
                     l1 = null;
                 }
             }
-            allNodes.add(smaller);
-            // same as l1 == null, cause when one listnode is null another listnode is null at same time
+            //allNodes.add(smaller);
+            // same as l1 == null,
+            // cause when one listnode is null another listnode is null at same time
             if (l2 == null) {
                 has = false;
             }
         }
         // set each node from all nodes, and move current pointer to next pointer
-        for (ListNode node : allNodes) {
-            current.next = node;
-            current = current.next;
-        }
+        //for (ListNode node : allNodes) {
+        //  current.next = node;
+        // current = current.next;
+        //}
         return sentinelNode.next;
     }
 }
